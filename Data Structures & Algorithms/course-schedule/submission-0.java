1bc1da392b8598200d5f20bed65dffc3e1c051a2
Parth Shrivastava <64528721+ParthShrivastava22@users.noreply.github.com>
2026-05-16 16:58:47 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+        for (int[] course: prerequisites) {
+            if (adjList.containsKey(course[0])) adjList.get(course[0]).add(course[1]);
+            else {
+                List<Integer> list = new ArrayList<>();
+                list.add(course[1]);
+                adjList.put(course[0], list);
+            }
+        }
+
+        Map<Integer, Integer> state = new HashMap<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            state.put(i, 0);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state.get(i) == 0) {
+                if (formCycle(prerequisites, state, i, adjList)) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean formCycle(int[][] prerequisites, Map<Integer, Integer> state, 
+                                int i, Map<Integer, List<Integer>> adjList) {
+        if (state.get(i) == 1) return true;
+        if (state.get(i) == 2) return false;
+        if (!adjList.containsKey(i)) return false;
+
+        state.put(i, 1);
+        List<Integer> list = adjList.get(i);
+        for (int course: list) {
+            if (formCycle(prerequisites, state, course, adjList)) return true;
+        }
+        state.put(i, 2);
+        
+        return false;
+    }
+}
