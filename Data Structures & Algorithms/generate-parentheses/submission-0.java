44c4865146292fb59c976830ab13dec123ced220
Parth Shrivastava <64528721+ParthShrivastava22@users.noreply.github.com>
2026-05-18 10:26:57 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        generateSequence(list, new ArrayList<>(), n, 0, 0);
+
+        return list;
+    }
+
+    private void generateSequence(List<String> list, List<Character> current, int n, 
+                    int open, int close) {
+        if (current.size() == n * 2) {
+            if (open == close) addToList(list, current);
+            return;
+        }
+
+        if (close < open) {
+            close++;
+            current.add(')');
+            generateSequence(list, current, n, open, close);
+            current.remove(current.size() - 1);
+            close--;
+        }
+
+        open++;
+        current.add('(');
+        generateSequence(list, current, n, open, close);
+        current.remove(current.size() - 1);
+        open--;
+    }
+
+    private void addToList(List<String> list, List<Character> current) {
+        StringBuilder builder = new StringBuilder();
+        for (char c: current) {
+            builder.append(c);
+        }
+
+        list.add(builder.toString());
+    }
+}
