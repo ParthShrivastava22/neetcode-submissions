8301654a237a29d6cf9a1c35c6a31e4ffa20aa27
Parth Shrivastava <64528721+ParthShrivastava22@users.noreply.github.com>
2026-07-25 16:51:50 +0530

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (foundWord(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean foundWord(char[][] board, String word, int i, int j, int index) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return false;
+        if (board[i][j] != word.charAt(index)) return false;
+
+        if (index == word.length() - 1) return true;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean up = foundWord(board, word, i - 1, j, index + 1);
+        if (up) return true;
+        boolean down = foundWord(board, word, i + 1, j, index + 1);
+        if (down) return true;
+        boolean left = foundWord(board, word, i, j - 1, index + 1);
+        if (left) return true;
+        boolean right = foundWord(board, word, i, j + 1, index + 1);
+        if (right) return true;
+
+        board[i][j] = temp;
+
+        return up || down || left || right;
+    }
+}
