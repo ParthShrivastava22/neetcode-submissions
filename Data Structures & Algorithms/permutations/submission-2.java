63f660b023f27ee55acfc404cc18c5767ad3a37b
Parth Shrivastava <64528721+ParthShrivastava22@users.noreply.github.com>
2026-07-25 15:01:33 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    List<List<Integer>> perms;
+
+    public List<List<Integer>> permute(int[] nums) {
+        perms = new ArrayList<>();
+        boolean[] chosen = new boolean[nums.length];
+        addToList(new ArrayList<>(), nums, chosen);
+        return perms;
+    }
+
+    private void addToList(List<Integer> list, int[] nums, boolean[] chosen) {
+        if (list.size() == nums.length) {
+            perms.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (chosen[i]) continue;
+
+            list.add(nums[i]);
+            chosen[i] = true;
+            addToList(list, nums, chosen);
+            list.remove(list.size() - 1);
+            chosen[i] = false;
+        }
+    }
+}
